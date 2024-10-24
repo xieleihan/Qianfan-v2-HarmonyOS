5c901780142932ac4d323453e8ee8096fcf761ae
南秋SouthAki <xieleihan@gmail.com>
2024-10-24 17:09:40 +0800

    feat:约束代码类型

---
+++ database.sql
@@ -0,0 +1,15 @@
+create database pythondb;
+use pythondb;
+
+-- 用户表 
+drop table if exists `user`;
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL, -- 用户名
+    useremail VARCHAR(100) NOT NULL, -- 用户邮箱
+    userpassword VARCHAR(100) NOT NULL, -- 用户密码
+    usertoken varchar(100) not null,
+    useravater text, -- 用户头像
+    useraddress varchar(20) -- 用户ip地址
+);
+select * from user;
